9fe2944e956daa1c30db629225a08d8107053df5
marnvel <marnvel.24@gmail.com>
2023-09-06 19:10:38 -0300

    Reorganización. 06-09-2023. 19:10hs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-// import './classes/Gemetry'
-// import './classes/TodoList'
-import './classes/Library'
-// import './classes/ContactList'
+// import './classes/1.ToDo/TodoList'
+// import './classes/2.Geometry/Gemetry.ts'
+import './classes/3.Library/Library'
+// import './classes/5.ContactManager/ContactList.ts'
 
 function main () {}
 
